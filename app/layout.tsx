8fd0b843e606db355fc57861790b759fb818e888
spi971 <sandypierre@protonmail.com>
2023-12-27 12:36:28 +0100

    [ADD]Modal base components, button component & fix bugs : Warning: Extra attributes from the server & use client

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 
@@ -16,9 +18,12 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning={true}>
 			<body className={nunitoFont.className}>
-				<Navbar />
+				<ClientOnly>
+					<Modal/>
+					<Navbar />
+				</ClientOnly>
 				{children}
 			</body>
 		</html>
